7e8d8d9dd786c6b764b43fa7b6cb24f47ee0df5f
Aayush Bhankale <abhankale@gmail.com>
2021-02-19 21:55:03 +0530

    feat: pug files added

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+app.set('view engine', 'pug'); // Sets data globally to Express application
+app.set('views', path.join(__dirname, "views")); 
+
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
@@ -15,7 +18,8 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.render('404')
 });
 
 app.listen(3000);
